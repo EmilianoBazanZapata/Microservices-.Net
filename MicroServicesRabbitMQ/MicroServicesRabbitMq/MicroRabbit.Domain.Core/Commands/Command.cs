2755a2264c08127c19f27b3f657115d06cb08b2d
Emiliano Bazan-Zapata <emilianobz546gmail.com>
2024-11-12 08:41:23 -0300

    Capa Dominio

---
+++ MicroServicesRabbitMQ/MicroServicesRabbitMq/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,13 @@
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Commands;
+
+public abstract class Command : Message
+{
+    public DateTime Timestamp { get; protected set; }
+
+    public Command()
+    {
+        Timestamp = DateTime.Now;
+    }
+}
