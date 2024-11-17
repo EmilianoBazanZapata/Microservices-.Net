299a662c7d759a755d79335bacf95091dc51a8e4
Emiliano Bazan-Zapata <emilianobz546gmail.com>
2024-11-17 12:48:50 -0300

    IoC Microservices

--- MicroServicesRabbitMQ/MicroServicesRabbitMq/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroServicesRabbitMQ/MicroServicesRabbitMq/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,3 +1,8 @@
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.Configuration;
@@ -13,5 +18,12 @@ public class DependencyContainer
         //Domain Bus
         services.AddTransient<IEventBus, RabbitMqBus>();
         services.Configure<RabbitMqSettings>(c => configuration.GetSection("RabbitMqSettings"));
+        
+        //Application Services
+        services.AddTransient<IAccountService, AccountService>();
+        
+        //Data
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
     }
 }
